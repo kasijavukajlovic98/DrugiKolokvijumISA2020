5a4f3ba174cb72c04f7f274ac477fa6614a92704
Kasija Vukajlovic <kasijavukajlovic98@gmail.com>
2020-06-10 12:51:46 +0200

    Brisanje starog

--- src/main/java/kolokvijum2/app/repository/GostRepository.java
+++
@@ -1,9 +0,0 @@
-package kolokvijum2.app.repository;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import kolokvijum2.app.model.Gost;
-
-@Repository
-public interface GostRepository extends CrudRepository<Gost, Long>{
-
-}
